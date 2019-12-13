632961263557c83a260c9b5769ab811ff41febc1
Student <example@example.com>
2019-12-13 12:13:04 -0800

    revert back on old commit.

--- Program.cs
+++ Program.cs
@@ -7,23 +7,28 @@ namespace Bakery
     {
         public static void Main()
         {
-            Console.WriteLine("Welcome to Pierre's Bakery! \n Cost of Bread: $5 \n Cost of Pastry: $2 \n Let's take a look at our specials menu? ");
+            Console.WriteLine("Welcome to Pierre's Bakery! \n ---------------------- \n Cost of Bread: $5 \n Cost of Pastry: $2");
+
             
-                Console.WriteLine("Today's Scpecials:\n Bread: buy 2 loaves get the 3rd free. \n Pastry: buy 1 for $2 or 3 for $5. ");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Today's Scpecials:\n ----------------------\nBread: buy 2 loaves get the 3rd free. \n Pastry: buy 1 for $2 or 3 for $5. ");
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("How many Pastry's would you like?");
+            int userPastry = int.Parse(Console.ReadLine());
+            Pastry customerPastry = new Pastry(userPastry, 2);
+            customerPastry.DeterminePricePastry(userPastry);
+
             
-                Console.WriteLine("How many loaves of Bread would you like?");
-                int userBread = int.Parse(Console.ReadLine());
-                Bread customerBread = new Bread(userBread, 2);
-                customerBread.DeterminePriceBread(userBread);
+            Console.WriteLine("How many loaves of Bread would you like?");
+            int userBread = int.Parse(Console.ReadLine());
+            Bread customerBread = new Bread(userBread, 2);
+            customerBread.DeterminePriceBread(userBread);
 
-                Console.WriteLine("How many Pastry's would you like?");
-                int userPastry = int.Parse(Console.ReadLine());
-                Pastry customerPastry = new Pastry(userPastry, 2);
-                customerPastry.DeterminePricePastry(userPastry);
-
-                // Console.WriteLine(Pastry.TotalPastry);
-
-        
+            int totalPrice = (customerPastry.DeterminePricePastry(userPastry) + customerBread.DeterminePriceBread(userBread));
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Total due: $" + totalPrice);
+   
         }
     }
 }
