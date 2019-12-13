ab672bb8c16f2d1040837df96a2ab6e2948204a8
Jamila <Jama@Mikhailas-MacBook-Pro.local>
2019-12-13 08:52:34 -0800

    create a prompt with the prices of the products and allows user to branch based on user input.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
 using System;
 using System.Collections.Generic;
 
-namespace 
+namespace Pastry.Bread
 {
     class Project 
     {
-        static void Maid()
+        public static void Main()
         {
-            
+            Console.WriteLine("Welcome to Pierre's Bakery! \n Cost of Bread: $5 \n Cost of Pastry: $3 \n Would you like to look at our specials menu?");
+            string scpecials = Console.ReadLine();
+            if (scpecials == "Yes")
+            {
+                Console.WriteLine("Today's Scpecials:\n Bread: buy 2 loaves of get the 3rd free \n Pastry: buy 1 for $2 or 3 for $5 ");
+            }
+            else
+            {
+                Console.WriteLine("What would you like to order?");
+            }
         }
     }
 }
