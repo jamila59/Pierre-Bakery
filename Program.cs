363b37de5d77be0d43ef1942aaf95ee3acad7f4e
Student <example@example.com>
2019-12-13 16:00:32 -0800

    oranganize the console further, complete README.

--- Program.cs
+++ Program.cs
@@ -31,55 +31,55 @@ namespace Bakery
         public static void Main()
         {
            
-            Console.WriteLine("Welcome to Pierre's Bakery! \n ---------------------- \n Cost of Bread: $5 \n Cost of Pastry: $2");
-            Console.WriteLine("----------------------");
-            Console.WriteLine("Today's Scpecials:\n ----------------------\nBread: buy 2 loaves get the 3rd free. \nPastry: buy 1 for $2 or 3 for $5. ");
-            Console.WriteLine("----------------------");
+            Console.WriteLine(" ---------------------- \n Welcome to Pierre's Bakery! \n ---------------------- \n Cost of Bread: $5.00 \n Cost of Pastry: $2.00");
+            Console.WriteLine(" ----------------------");
+            Console.WriteLine(" Today's Scpecials:\n ----------------------\n Bread: buy 2, loaves get the 3rd free. \n Pastry: buy 1, for $2.00 or 3 for $5.00. ");
+            Console.WriteLine(" ----------------------");
 
 
-            Console.WriteLine("Which pastry would you like? (0-4)");
+            Console.WriteLine(" Which pastry would you like? (0-4)");
             foreach (KeyValuePair<int, string> pastry in pastrys)  
             {  
-                Console.WriteLine("Key: {0}, Value: {1}",  
+                Console.WriteLine(" Key: {0}, Value: {1}",  
                 pastry.Key, pastry.Value);
             }  
             int flovorPastry = int.Parse(Console.ReadLine());  
-            Console.WriteLine("How many Pastry's would you like?");
+            Console.WriteLine(" How many Pastry's would you like?");
             int userPastry = int.Parse(Console.ReadLine());
             Pastry customerPastry = new Pastry(userPastry, 2);
             customerPastry.DeterminePricePastry(userPastry);
             
 
-            Console.WriteLine("Which drink would you like? (0-4)");
+            Console.WriteLine(" Which drink would you like? (0-4)");
             foreach (KeyValuePair<int, string> drink in drink)  
             {  
-                Console.WriteLine("Key: {0}, Value: {1}",  
+                Console.WriteLine(" Key: {0}, Value: {1}",  
                 drink.Key, drink.Value);
             }  
             int flovorDrink = int.Parse(Console.ReadLine()); 
-            Console.WriteLine("How many Drink's would you like?");
+            Console.WriteLine(" How many Drink's would you like?");
             int userDrink = int.Parse(Console.ReadLine()); 
             Drink customerDrink = new Drink(userDrink, 2);
             customerDrink.DeterminePriceDrink(userDrink);
 
-            Console.WriteLine("Which bread would you like? (0-4)");
+            Console.WriteLine(" Which bread would you like? (0-4)");
             foreach (KeyValuePair<int, string> bread in bread)  
             {  
-                Console.WriteLine("Key: {0}, Value: {1}",  
+                Console.WriteLine(" Key: {0}, Value: {1}",  
                 bread.Key, bread.Value);
             } 
             int flovorBread = int.Parse(Console.ReadLine());  
-            Console.WriteLine("How many loaves of Bread would you like?");
+            Console.WriteLine(" How many loaves of Bread would you like?");
             int userBread = int.Parse(Console.ReadLine());
             Bread customerBread = new Bread(userBread, 2);
             customerBread.DeterminePriceBread(userBread);
 
             int totalPrice = (customerPastry.DeterminePricePastry(userPastry) + customerBread.DeterminePriceBread(userBread)+customerDrink.DeterminePriceDrink(userDrink));
-            Console.WriteLine("----------------------");
+            Console.WriteLine(" ----------------------");
             Console.WriteLine("You hava ordered: " + "" + userBread + " " + bread[flovorBread] + "(s)");
             Console.WriteLine("You hava ordered: " + "" + userPastry + " " + pastrys[flovorPastry] + "(s)");
             Console.WriteLine("You hava ordered: " + "" + userDrink + " " + drink[flovorPastry] + "(s)");
-            Console.WriteLine("Total due: $" + totalPrice);
+            Console.WriteLine("Total due: $" + totalPrice+ ".00");
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,28 +1,28 @@
 
  # _Pierre-Bakery_
 
-#### _Application takes user order and prints back a reciept with items the user ordered and a total amount due._ {Dec 13,2019}_
+#### _Application takes user order and prints back a reciept with items the user ordered and a total amount due._     [Dec 13,2019]
 
 #### By _**Jamila Aliyeva**_
 
 ## Description
 
-_{A console application that sends out a prompts welcoming the user, taking  the users Coffee order, Pastry order and Bread order. Then, the console prints back the quanity of each type or order along with the type of option for the order the selected (ex. Black Tea) and prints out total amount due. }_
+_A console application that sends out a prompts welcoming the user, taking  the users Coffee order, Pastry order and Bread order. Then, the console prints back the quanity of each type or order along with the type of option for the order the selected (ex. Black Tea) and prints out total amount due._
 
  ## Specs
 
  * _When "dotnet run" is input 
     -> 
-    prompt comes up with cost per bread and cost per pastry_ *
+    prompt comes up with cost per bread and cost per pastry_ 
  * _Application offers deals
     ->
     Bread: Buy 2 get 1 free. 
-    Pastry: Buy 1 for $2 for $5_ *
+    Pastry: Buy 1 for $2 for $5_ 
  * _user can specify quantity of loaves of Bread and amt of Pastry ->
-    Pastry: 2  Bread: 5_ *
+    Pastry: 2  Bread: 5_ 
  * _user recieves total cost of order 
     -> 
-    Total: $14.00_ *
+    Total: $14.00_ 
 
 
 ## Setup/Installation Requirements
@@ -33,7 +33,19 @@ _{A console application that sends out a prompts welcoming the user, taking  the
 
 ## Technologies Used
 
-_{C#, .NET}_
+_C#, .NET_
+`
+### MIT License
+Copyright (c) 2019 Jamila Aliyeva
+
+*Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*
+
+
+Copyright (c) 2016 **_Jamila Aliyeva_**
 
 
 
